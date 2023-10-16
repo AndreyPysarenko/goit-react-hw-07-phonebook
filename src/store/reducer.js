3321f8d25b93321dc1fb9d7a1f15b8af74568671
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-10-16 16:20:00 +0300

    3

--- src/store/reducer.js
+++ src/store/reducer.js
@@ -1,18 +1,9 @@
-import storage from 'redux-persist/lib/storage';
-import { persistReducer } from 'redux-persist';
 import { contactsReducer } from './contactsSlice';
 import { filterReducer } from './filterSlice';
 import { appReducer } from './appState/slice';
 
-const persistConfig = {
-  key: 'contacts',
-  storage,
-};
-
-const persistedReducer = persistReducer(persistConfig, contactsReducer);
-
 export const reducer = {
-  contacts: persistedReducer,
+  contacts: contactsReducer,
   filter: filterReducer,
   appState: appReducer,
 };
