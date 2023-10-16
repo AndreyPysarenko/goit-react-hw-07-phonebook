d9eef648c51d9914cab48688dc3756a0ccee34b3
AndreyPysarenko <114475908+AndreyPysarenko@users.noreply.github.com>
2023-10-16 15:51:57 +0300

    fix2

--- src/components/App.jsx
+++ src/components/App.jsx
@@ -3,11 +3,13 @@ import Filter from './Filter';
 import ContactList from './ContactList';
 import { Container } from './App.styled';
 import { useEffect } from 'react';
-import { useDispatch } from 'react-redux';
+import { useDispatch, useSelector } from 'react-redux';
 import { fetchContacts } from 'store/operations';
 import Loader from './Loader';
+import { selectContacts } from 'store/selector';
 
 const App = () => {
+  const contacts = useSelector(selectContacts);
   const dispatch = useDispatch();
 
   useEffect(() => {
@@ -21,8 +23,8 @@ const App = () => {
         <h1>Phonebook</h1>
         <ContactForm />
         <h2>Contacts</h2>
-        <Filter />
-        <ContactList />
+        {contacts.length > 0 ? (<Filter />) : (<p>Your Phone book is empty!</p>)}
+        {contacts.length > 0 && <ContactList />}
       </Container>
     </>
   );
